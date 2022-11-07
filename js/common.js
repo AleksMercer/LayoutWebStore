2e10aaa49a91bff78268c7ef8479e7e3371fb59d
Alex Mercer <kalagaev.alex.a@gmail.com>
2022-11-07 20:56:59 +0300

    07.11 / header menu burger (html + css + js)

--- js/common.js
+++ js/common.js
@@ -28,15 +28,42 @@ openCloseBtn.forEach(openClose => {
 
 const searchButton = document.querySelector('.search-button');
 
-searchButton.addEventListener('click', () => {
+searchFunc = (e) => {
   document.getElementById('search-input').classList.toggle('active');
-})
+}
+
+searchButton.addEventListener('click', searchFunc)
 
 //——————————————————————————————————————————————————————————————————————————————
 
 //———Header-menu————————————————————————————————————————————————————————————————
 
+const headerMenu = document.querySelector('.header__menu');
+const headerMenuBtn = document.querySelector('.header-burger-btn');
+const headerMenuLink = document.querySelectorAll('.header__menu > a')
 
+const headerBurger = document.querySelector('.header__menu-block');
+const headerBurgerLink = document.querySelector('.menu-block-link');
+const headerBurgerLinkFirst = document.querySelector('.menu-block-link > a');
+
+const headerMenuFunc = () => {
+  let headerMenuLinkVar = document.querySelectorAll('.header__menu > a')
+
+  headerBurger.classList.toggle('active');
+  headerMenuBtn.classList.toggle('active');
+  document.body.classList.toggle('active');
+  
+  headerMenuLink.forEach(e => {
+    if (headerMenuLinkVar.length >= 3) {
+      headerBurgerLinkFirst.before(e) 
+    } else if (headerMenuLinkVar.length < 3) {
+      headerMenu.append(e) 
+    } 
+  })
+}
+
+headerMenuBtn.addEventListener('click', headerMenuFunc)
+headerBurger.addEventListener('click', headerMenuFunc)
 
 //——————————————————————————————————————————————————————————————————————————————
 
